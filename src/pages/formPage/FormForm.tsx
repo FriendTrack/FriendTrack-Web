b4173ee0cb69784edf57a86bb26f0d841e3049dd
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-06-03 15:12:56 +0700

    FIxes

--- src/pages/formPage/FormForm.tsx
+++ src/pages/formPage/FormForm.tsx
@@ -1,62 +1,66 @@
 import { Button } from "@/components/ui/button";
-import { CardTitle, CardHeader, CardContent, CardDescription } from "@/components/ui/card";
-import { Select } from "@/components/ui/select";
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Select } from "@/components/ui/select";
 import { Label } from "@radix-ui/react-label";
 import React from "react";
 
 const FormForm: React.FC<{ className?: string }> = () => {
-    const [inputs, setInputs] = React.useState<string[]>([""]);
-  
-    const handleAddInput = () => {
-      setInputs([...inputs, ""]);
-    };
-  
-    const handleInputChange = (index: number, value: string) => {
-      const newInputs = [...inputs];
-      newInputs[index] = value;
-      setInputs(newInputs);
-    };
-  
-    return (
-      <CardContent>
-        <Label>Ваше настроение сегодня:</Label>
-        <Select
-          options={[
-            { value: "good", label: "Хорошее" },
-            { value: "neutral", label: "Нейтральное" },
-            { value: "bad", label: "Плохое" },
-          ]}
-        />
-        <CardHeader>
-          <CardTitle>С кем вы сегодня общались</CardTitle>
-        </CardHeader>
-        <CardDescription style={{ fontSize: "18px" }}>
-          Постарайтесь вводить имена в том порядке, в котором вспоминаете их. Скорее всего запомнившиеся люди внесли
-          больший вклад в ваше настроение сегодня.
-        </CardDescription>
-        <div style={{ maxHeight: "500px", overflowY: "auto" }}>
+  const [inputs, setInputs] = React.useState<string[]>([""]);
+
+  const handleAddInput = () => {
+    setInputs([...inputs, ""]);
+  };
+
+  const handleInputChange = (index: number, value: string) => {
+    const newInputs = [...inputs];
+    newInputs[index] = value;
+    setInputs(newInputs);
+  };
+
+  return (
+    <CardContent>
+      <Label>Ваше настроение сегодня:</Label>
+      <Select
+        options={[
+          { value: "good", label: "Хорошее" },
+          { value: "neutral", label: "Нейтральное" },
+          { value: "bad", label: "Плохое" },
+        ]}
+      />
+      <CardHeader>
+        <CardTitle>С кем вы сегодня общались</CardTitle>
+      </CardHeader>
+      <CardDescription>
+        Постарайтесь вводить имена в том порядке, в котором вспоминаете их.
+        Скорее всего запомнившиеся люди внесли больший вклад в ваше настроение
+        сегодня.
+      </CardDescription>
+      <div>
         {inputs.map((value, index) => (
           <Input
             key={index}
-            style={{ marginTop: index === 0 ? "25px" : "15px" }}
             placeholder="Имя"
             name="name"
             value={value}
+            className="mt-3"
             onChange={(e) => handleInputChange(index, e.target.value)}
           />
         ))}
-        </div>
-  
-        <Button className="w-full md:w-auto" onClick={handleAddInput} style={{ width: "100%", backgroundColor: "gray", marginTop: "20px" }}>
-          Добавить
-        </Button>
-  
-        <Button className="w-full md:w-auto" style={{ marginTop: "25px" }}>
-          Готово
-        </Button>
-      </CardContent>
-    );
+      </div>
+
+      <Button className="w-full bg-gray-500 mt-3" onClick={handleAddInput}>
+        Добавить
+      </Button>
+
+      <Button className="w-full md:w-auto mt-3 bg-green-700">Готово</Button>
+    </CardContent>
+  );
 };
 
 export default FormForm;
--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,46 +1,62 @@
 import { ROUTES } from "@/lib/constants/router";
 import React, { ReactNode } from "react";
 import { NavLink } from "react-router-dom";
-import useAuth from '/src/Auth';
-import Form from '/public/assets/drawerSVGs/form.svg';
-import Top from '/public/assets/drawerSVGs/trophy.svg';
-import Find from '/public/assets/drawerSVGs/compass.svg';
-import Analysis from '/public/assets/drawerSVGs/trending-up.svg';
-import "./Layout.css"
+import "./Layout.css";
+import Find from "/assets/drawerSVGs/compass.svg";
+import Form from "/assets/drawerSVGs/form.svg";
+import Analysis from "/assets/drawerSVGs/trending-up.svg";
+import Top from "/assets/drawerSVGs/trophy.svg";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  var isAuthenticated = true;
-  // const { isAuthenticated } = useAuth();
+  var isAuthenticated = false;
 
   return (
     <main className="w-screen h-screen flex flex-col lg:flex-row">
-      <aside className="w-full mb-5 lg:mb-0  lg:w-3/12 xl:w-2/12 border border-solid border-slate-300 rounded-e-md" style={{backgroundColor:"rgb(5, 30, 40)"}}>
-        <ul className="flex gap-3 lg:flex-col items-center text-xl">
-          { isAuthenticated ?
-            (<>
+      <aside
+        className="w-full mb-5 lg:mb-0 text-gray-400  lg:w-2/12 xl:w-1/12 border border-solid border-slate-300 rounded-e-md"
+        style={{ backgroundColor: "rgb(5, 30, 40)" }}
+      >
+        <ul className="flex h-full lg:flex-col items-center justify-evenly text-xl">
+          {isAuthenticated ? (
+            <>
               <li>
-                <NavLink to={/*</li>ROUTES.LOGOUT*/""}>Выйти</NavLink>
+                <NavLink to={/*</li>ROUTES.LOGOUT*/ ""}>Выйти</NavLink>
               </li>
-              <div style={{marginTop:"30px", marginBottom:"30px", width:"100%", height:"5px", backgroundColor:"gray"}}></div>
-              <li><NavLink to={ROUTES.FORM} className={({isActive}) => isActive ? "active": "" }><img src={Form} style={{height:"100px", width:"100px", marginTop:"50px", color:"inherit"}} /></NavLink></li>
-              <li><NavLink to={ROUTES.FORM}><img src={Top} style={{height:"100px", width:"100px", marginTop:"70px"}} /></NavLink></li>
-              <li><NavLink to={ROUTES.FORM}><img src={Analysis} style={{height:"100px", width:"100px", marginTop:"70px"}} /></NavLink></li>
-              <li><NavLink to={ROUTES.FORM}><img src={Find} style={{height:"100px", width:"100px", marginTop:"70px"}} /></NavLink></li>
-             </>
-            ) 
-              : 
-            (
-              <>
+
               <li>
-                <NavLink to={ROUTES.LOGIN}>Войти</NavLink>
+                <NavLink
+                  to={ROUTES.FORM}
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  <img src={Form} />
+                </NavLink>
               </li>
               <li>
-                <NavLink to={ROUTES.REGISTRATION}>Зарегистрироваться</NavLink>
+                <NavLink to={ROUTES.FORM}>
+                  <img src={Top} />
+                </NavLink>
               </li>
-              <div style={{marginTop:"30px", marginBottom:"30px", width:"100%", height:"5px", backgroundColor:"gray"}}></div>
-              </>
-            )
-          }
+              <li>
+                <NavLink to={ROUTES.FORM}>
+                  <img src={Analysis} />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={ROUTES.FORM}>
+                  <img src={Find} />
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <div>
+              <li>
+                <NavLink to={ROUTES.LOGIN}>Вход</NavLink>
+              </li>
+              <li>
+                <NavLink to={ROUTES.REGISTRATION}>Регистрация</NavLink>
+              </li>
+            </div>
+          )}
         </ul>
       </aside>
       {children}
