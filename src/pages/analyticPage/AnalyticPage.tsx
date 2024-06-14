62ec1a4297ca924ef8646294e2759b93455acf1c
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-06-14 16:36:52 +0700

    Fix
    - adaptive for Radar

--- src/pages/analyticPage/AnalyticPage.tsx
+++ src/pages/analyticPage/AnalyticPage.tsx
@@ -1,23 +1,38 @@
-import { Radar } from 'react-chartjs-2';
-import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement } from 'chart.js';
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
 
 ChartJS.register(RadialLinearScale, PointElement, LineElement);
 
 const AnalyticPage = () => {
   const data = {
-    labels: ["Коммуникация", "Уважение", "Доверие", "Времяпрепровождение", "Эмпатия"],
+    labels: [
+      "Коммуникация",
+      "Уважение",
+      "Доверие",
+      "Времяпрепровождение",
+      "Эмпатия",
+    ],
     datasets: [
       {
         label: "Student A",
         data: [2, 3, 5, 5, 4],
         backgroundColor: "blue",
-      }
+      },
     ],
   };
   return (
-    <Radar data={data} />
+    <div>
+      <Radar
+        className="md:w-[450px] md:h-[450px] 2xl:w-[600px] 2xl:h-[600px]"
+        data={data}
+      />
+    </div>
   );
 };
 
-  
-  export default AnalyticPage;
+export default AnalyticPage;
