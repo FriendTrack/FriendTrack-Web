0fc3302820c5ddb56d66b9ad1d3cc15babcb442b
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-06-21 19:02:45 +0700

    Add:
    -captcha on login & register

--- src/pages/registrationPage/RegistrationForm.tsx
+++ src/pages/registrationPage/RegistrationForm.tsx
@@ -9,13 +9,17 @@ import {
 import { Input } from "@/components/ui/input";
 import { ROUTES } from "@/lib/constants/router";
 import { Label } from "@radix-ui/react-label";
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const RegistrationForm: React.FC<{ className?: string }> = ({ className }) => {
   const navigate = useNavigate();
+  const [token, setToken] = useState<string | null>(null);
 
-  const onRegisterClick = () => {
+  const onRegisterClick = (e: FormEvent) => {
+    e.preventDefault();
+    if (token === null) return;
     navigate("/");
   };
 
@@ -25,7 +29,7 @@ const RegistrationForm: React.FC<{ className?: string }> = ({ className }) => {
         <CardTitle>Регистрация</CardTitle>
       </CardHeader>
       <CardContent>
-        <form id="register_form" onSubmit={() => onRegisterClick()}>
+        <form id="register_form" onSubmit={(e) => onRegisterClick(e)}>
           <Label htmlFor="email">Email</Label>
           <Input type="email" placeholder="email" id="email" />
           <Label htmlFor="password" className="block mt-3">
@@ -41,6 +45,11 @@ const RegistrationForm: React.FC<{ className?: string }> = ({ className }) => {
             id="confirm_password"
           />
         </form>
+        <ReCAPTCHA
+          onChange={(token) => setToken(token)}
+          className="mt-3"
+          sitekey="6LdCOv4pAAAAANL1-hNs7D7d5pzjz2xNjbR2QbNj"
+        />
       </CardContent>
       <CardFooter className="h-full flex-col  gap-3 justify-between md:flex-row">
         <Button form="register_form" className="w-full md:w-auto">
