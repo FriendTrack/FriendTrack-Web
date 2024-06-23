fa410dad866230bb292c135c37fe252839a2cc19
Arseniy Dolganov <arsen190104@gmail.com>
2024-06-23 16:39:09 +0700

    Добавил выбор друга на страницу аналитики

--- src/pages/analyticPage/AnalyticPage.tsx
+++ src/pages/analyticPage/AnalyticPage.tsx
@@ -1,4 +1,5 @@
 import AnalyticWindow from './Components/AnalyticWindow'
+import { Contact } from '@/lib/api/requests/contact'
 
 export interface QualitiesDevelopment {
 	empathy: number[]
@@ -23,6 +24,26 @@ const qualitiesDevelopment: QualitiesDevelopment = {
 	trust: [1, 1, 1, 3, 4],
 	dates: ['22.06', '23.06', '24.06', '25.06', '26.06'],
 }
+
+const friends: Contact[] = [
+	{
+		name: "asldfkj",
+		details: "asldfkj",
+		link: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftrikky.ru%2Fkrasivye-kartinki-880042.html&psig=AOvVaw30GITpDKLzWxCkILtl0bag&ust=1719220729924000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCODa9biy8YYDFQAAAAAdAAAAABAE",
+		birthDate: "asldfkj",
+		id: "1",
+		userId: "asldfkj",
+	},
+	{
+		name: "arsen",
+		details: "asldfkj",
+		link: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fru.pinterest.com%2Fekaterinamec%2F%25D0%25BA%25D1%2580%25D0%25B0%25D1%2581%25D0%25B8%25D0%25B2%25D1%258B%25D0%25B5-%25D0%25B4%25D0%25B5%25D1%2582%25D1%2581%25D0%25BA%25D0%25B8%25D0%25B5-%25D0%25BA%25D0%25B0%25D1%2580%25D1%2582%25D0%25B8%25D0%25BD%25D0%25BA%25D0%25B8%2F&psig=AOvVaw30GITpDKLzWxCkILtl0bag&ust=1719220729924000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCODa9biy8YYDFQAAAAAdAAAAABAJ",
+		birthDate: "asldfkj",
+		id: "1",
+		userId: "asldfkj",
+	},
+]
+
 const contactsCount: ContactsCount = {
 	positive: [1, 2, 4, 7, 13, 18, 20, 21, 21],
 	negative: [1, 2, 3, 3, 5, 4, 3, 3, 2],
@@ -47,6 +68,7 @@ export const AnalyticPage = () => {
 				<AnalyticWindow
 					contactsCount={contactsCount}
 					qualitiesDevelopment={qualitiesDevelopment}
+					friends={friends}
 				/>
 			</div>
 		</main>
